7e1902dcdc92dae8db0c12bfe09682793a30f5ee
ideadesignmedia <60307610+ideadesignmedia@users.noreply.github.com>
2021-11-10 15:46:45 -0700

    Auto-create .env file

--- env.js
+++ env.js
@@ -1,6 +1,8 @@
-const { readFileSync } = require('fs')
+const { readFileSync, existsSync, writeFileSync } = require('fs')
 const { join } = require('path')
-let config = readFileSync(join(process.cwd(), '.env')).toString('utf-8').concat().trim().split('=').map(u => u.replace(/[\n\r]/g, '=')).map(u => u.split('=')).reduce((a, b) => {
+const path = join(process.cwd(), '.env')
+if (!existsSync(path)) writeFileSync(path, '')
+let config = readFileSync(path).toString('utf-8').concat().trim().split('=').map(u => u.replace(/[\n\r]/g, '=')).map(u => u.split('=')).reduce((a, b) => {
     for (let i = 0; i < b.length; i++) if (b[i]) a.push(b[i].trim())
     return a
 }, [])
